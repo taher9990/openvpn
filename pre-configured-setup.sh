5025361f2b425675d1119d61ecac5a2060bd4ab5
TAHER A.BAHASHWAN <taher9990@hotmail.com>
2022-01-08 10:46:07 +0300

    Update pre-configured-setup.sh

--- pre-configured-setup.sh
+++ pre-configured-setup.sh
@@ -1,1 +1,12 @@
-
+sudo apt update && sudo apt upgrade
+apt install -y openvpn
+wget https://git.io/vpn -O openvpn-install.sh
+chmod +x openvpn-install.sh
+sudo ./openvpn-install.sh
+cd  /etc/openvpn/server/
+cp /root/client.ovpn /etc/openvpn/server/
+cat >> /etc/sysctl.conf << EOF
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+EOF

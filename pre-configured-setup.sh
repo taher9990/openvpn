268383fd99a360512a92b17b9c0f0550db1e653d
TAHER A.BAHASHWAN <taher9990@hotmail.com>
2022-01-08 10:55:20 +0300

    Update pre-configured-setup.sh

--- pre-configured-setup.sh
+++ pre-configured-setup.sh
@@ -5,8 +5,17 @@ chmod +x openvpn-install.sh
 sudo ./openvpn-install.sh
 cd  /etc/openvpn/server/
 cp /root/client.ovpn /etc/openvpn/server/
+
+systemctl enable openvpn-server@server.service
+systemctl status openvpn-server@server.service
+
 cat >> /etc/sysctl.conf << EOF
 net.ipv4.ip_forward = 1
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.send_redirects = 0
 EOF
+
+#Generate new password:
+#tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '' 
+#useradd -p $(openssl passwd -1 LCQzFokjlK) user1
+#useradd -p $(openssl passwd -1 XjBuwFeiQs) user2

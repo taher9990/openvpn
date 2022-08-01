a7fe6152350b6969f66c17cf9e2074b5086010f8
TAHER A.BAHASHWAN <taher9990@hotmail.com>
2022-08-01 12:51:24 +0300

    Update and rename installation_steps to installation.sh

---
+++ installation.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade
+cat >> /etc/sysctl.conf << EOF
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+EOF
+apt install -y openvpn 
+wget https://git.io/vpn -O openvpn-install.sh
+chmod +x openvpn-install.sh
+sudo ./openvpn-install.sh
+cd  /etc/openvpn/server/
+cp /root/client.ovpn /etc/openvpn/server/
